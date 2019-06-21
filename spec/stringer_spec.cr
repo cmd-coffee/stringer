79f66f184efabef61494f041a359f5156c337a59
Evan Closson <evan@evanclosson.com>
2019-06-21 18:08:33 -0400

    Added #truncate and #squish

--- spec/stringer_spec.cr
+++ spec/stringer_spec.cr
@@ -1,9 +1,67 @@
 require "./spec_helper"
 
 describe Stringer do
-  # TODO: Write tests
+  it "is included in String" do
+    "foo-bar".should be_a(Stringer)
+  end
 
-  it "works" do
-    false.should eq(true)
+  describe "#truncate" do
+    it "has examples" do
+      strum = "The quick brown fox jumps over the lazy dog"
+      strum.truncate(20).should eq("The quick brown f...")
+      strum.truncate(20, {:omission => "!!!"}).should eq("The quick brown f!!!")
+      strum.truncate(20, {:separator => " "}).should eq("The quick brown...")
+    end
+
+    it "is unmodified when size is less than truncation length" do
+      "foo-bar".truncate(10).should eq("foo-bar")
+    end
+
+    it "is unmodified when size is the same as truncation length" do
+      "foo-bar".truncate(7).should eq("foo-bar")
+    end
+
+    it "is same when not truncated" do
+      actual = "foo-bar"
+      actual.truncate(7).should be(actual)
+    end
+
+    it "includes only omission when it's size is greater than truncation length" do
+      "foo-bar".truncate(2).should eq(Stringer::DEFAULT_TRUNCATE_OMISSION)
+    end
+
+    it "truncates the string and replaces with omission" do
+      "foo-bar-baz".truncate(10).should eq("foo-bar#{Stringer::DEFAULT_TRUNCATE_OMISSION}")
+    end
+
+    it "truncates the string and replaces with provided omission" do
+      options = {:omission => "... (more)"}
+      "foo-bar-baz-biz-bat-boo".truncate(20, options).should eq("foo-bar-ba... (more)")
+    end
+
+    it "truncates at separator break" do
+      options = {:separator => " "}
+      "foo bar baz biz bat boo".truncate(20, options).should eq("foo bar baz biz...")
+    end
+
+    it "truncates at separator break and replaces with provided omission" do
+      options = {:separator => "-", :omission => "... (more)"}
+      "foo-bar-baz-biz-bat-boo".truncate(20, options).should eq("foo-bar... (more)")
+    end
+  end
+
+  describe "#squish" do
+    it "trims and replaces all whitespace with a single space" do
+      "  some \t\n messy\ttxt \n\t   ".squish.should eq("some messy txt")
+    end
+
+    it "works with slready well formated string" do
+      "looks good".squish.should eq("looks good")
+    end
+
+    it "works with empty string" do
+      "".squish.should eq("")
+    end
+
   end
 end
--- src/stringer.cr
+++ src/stringer.cr
@@ -1,6 +1,62 @@
-# TODO: Write documentation for `Stringer`
+# Stringer (\ˈstriŋ-ər\) - one that strings.
+#
+# Useful utilities patched on top of the lovely `String`. Getting started is
+# easy! Just add the `Stringer` shard as a dependency and then it's as simple
+# as strumming along:
+#
+# ```
+# strum = "The quick brown fox jumps over the lazy dog"
+# strum.truncate(28) # => "The quick brown fox jumps..."
+# ```
 module Stringer
-  VERSION = "0.1.0"
+  DEFAULT_TRUNCATE_OMISSION = "..."
 
-  # TODO: Put your code here
+  # Returns a string truncated at the specified *length*.
+  #
+  # Options:
+  #   - :omission => postfix applied to the truncated string (default "...")
+  #   - :separator => separator to use for clean breaks (default nil)
+  #
+  # Examples:
+  #
+  # ```
+  # strum = "The quick brown fox jumps over the lazy dog"
+  # strum.truncate(20) #=> "The quick brown f..."
+  # strum.truncate(20, {:omission => "!!!"}) #=> "The quick brown f!!!"
+  # strum.truncate(20, {:separator => " "}) #=> "The quick brown..."
+  # ```
+  #
+  # Hat tip to Ruby on Rails inflections [String#truncate](https://api.rubyonrails.org/classes/String.html#method-i-truncate)
+  def truncate(length, options = {} of Symbol => String)
+    return self if size <= length
+
+    omission = options[:omission]? || DEFAULT_TRUNCATE_OMISSION
+    length_with_omission = length - omission.size
+
+    return omission if length_with_omission <= 0
+
+    stop = rindex(options[:separator], length_with_omission) if options[:separator]?
+    stop ||= length_with_omission
+
+    "#{self[0, stop]}#{omission}"
+  end
+
+  # Trims leading/trailing whitespace and compacts internal whitespace
+  #
+  # Examples:
+  #
+  # ```
+  # strum = "  some \t\n messy\ttxt \n\t   "
+  # strum.squish #=> "some messy txt"
+  # ```
+  #
+  # Hat tip to Ruby on Rails inflections [String#squish](https://api.rubyonrails.org/classes/String.html#method-i-squish)  
+  def squish
+    self.strip.gsub(/\s+/, ' ')
+  end
+end
+
+# :nodoc:
+class String
+  include Stringer
 end
